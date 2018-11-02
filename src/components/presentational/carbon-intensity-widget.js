9939acd35446bd5cf07484c61d56b23528e6b292
Reid Miller <rmiller+github@fleetcarma.com>
2018-11-02 13:56:19 -0400

    Added some more intensity metadata.
    5 minutes before presentation, as is software engineering tradition.

--- src/components/presentational/carbon-intensity-widget.js
+++ src/components/presentational/carbon-intensity-widget.js
@@ -5,11 +5,16 @@ import FormattedCarbonIntensity from './formatted-carbon-intensity';
 const CarbonIntensityWidget = ({carbonIntensity}) => {
     let requestPending = (carbonIntensity && carbonIntensity.pending) ? 'true' : 'false';
     let actualIntensity = carbonIntensity && carbonIntensity.details && carbonIntensity.details.intensity && carbonIntensity.details.intensity.actual;
+    let intensityIndex = carbonIntensity && carbonIntensity.details && carbonIntensity.details.intensity && carbonIntensity.details.intensity.index;
+    let validFrom = carbonIntensity && carbonIntensity.details && carbonIntensity.details.from;
+    let validTo = carbonIntensity && carbonIntensity.details && carbonIntensity.details.to;
 
     return (
         <ul>
             <li>Current Intensity: <FormattedCarbonIntensity co2Intensity={actualIntensity} /></li>
             <li>Request Pending: {requestPending}</li>
+            <li>Index: {intensityIndex}</li>
+            <li>Valid From: {validFrom} until {validTo}</li>
         </ul>
     );
 }
