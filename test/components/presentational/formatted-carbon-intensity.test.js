4b54e024a9815db4a61d0d25f622158f142a83ca
Reid Miller <rmiller+github@fleetcarma.com>
2018-11-01 23:02:27 -0400

    Added unit tests for numeric carbon intensity formatting.

--- test/components/presentational/formatted-carbon-intensity.test.js
+++ test/components/presentational/formatted-carbon-intensity.test.js
@@ -9,20 +9,40 @@ configure({ adapter: new Adapter() });  // could be moved to a test setup script
 describe('Formatted Carbon Intensity Text', () => {
     /*
      * The enzyme selector makes this test a bit brittle. What if we re-style and switch id attributes around?
-     * It does read clearer than the latter tests though.
+     * It does read clearer than the latter tests.
+     * If the test is broken, mocha also gives a more meaningful error message.
      */
-    it('should display "Unknown" if CO2 intensity is undefined (brittle).', () => {
+    it('should display "Unknown" if CO2 intensity is undefined (brittle)', () => {
         let formattedCarbonIntensity = shallow(<FormattedCarbonIntensity co2Intensity={false} />);
         expect(formattedCarbonIntensity.find('#formatted-co2-intensity').text()).to.equal('Unknown');
     });
 
-    it('should display "Unknown" if CO2 intensity is undefined.', () => {
+    it('should display "Unknown" if CO2 intensity is undefined', () => {
         let formattedCarbonIntensity = shallow(<FormattedCarbonIntensity co2Intensity={undefined} />);
         expect(formattedCarbonIntensity.children().someWhere(n => n.text() === 'Unknown')).to.equal(true);
     });
 
-    it('should display "Unknown" if CO2 intensity is falsy.', () => {
+    it('should display "Unknown" if CO2 intensity is falsy', () => {
         let formattedCarbonIntensity = shallow(<FormattedCarbonIntensity co2Intensity={false} />);
         expect(formattedCarbonIntensity.children().someWhere(n => n.text() === 'Unknown')).to.equal(true);
     });
+
+    /*
+     * The enzyme selector makes this test a bit brittle. What if we re-style and switch id attributes around?
+     * It does read clearer than the latter tests.
+     * If the test is broken, mocha also gives a more meaningful error message.
+     */
+    it('should append units-of-measure to numeric carbon intensity (brittle)', ()=> {
+        let testIntensity = 133.7;
+        let formattedCarbonIntensity = shallow(<FormattedCarbonIntensity co2Intensity={testIntensity} />);
+        expect(formattedCarbonIntensity.find('#formatted-co2-intensity').text()).to.equal(testIntensity + ' gCO2/kWh');
+    });
+
+    /* ¯\_(ツ)_/¯
+    it('should append units-of-measure to numeric carbon intensity', ()=> {
+        let testIntensity = 133.7;
+        let formattedCarbonIntensity = shallow(<FormattedCarbonIntensity co2Intensity={testIntensity} />);
+        expect(formattedCarbonIntensity.children().someWhere(n => n.text() === testIntensity + ' gCO2/kWh')).to.equal(true);
+    });
+    */
 });
